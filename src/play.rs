6c6cc988e40af4774fa73bd30adbfc9847b28a96
Nevin Flanagan <nevin@playsmith.net>
2024-05-30 12:26:30 -0400

    Added basic motion and facing tracking to Play.

--- src/play.rs
+++ src/play.rs
@@ -3,20 +3,26 @@ use crate::Rect;
 use super::{Input, Outcome, room::{Room, Enemy}, Side, Object};
 use std::{collections::BTreeSet, num::NonZeroU16};
 
+const MAX_THRUST: i16 = 5;
+
 pub struct Play<'a> {
     room: &'a Room,
     items: BTreeSet<usize>,
     facing: Side,
     player_h: u16,
     player_v: u16,
+    motion_h: i16,
+    motion_v: i16,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub enum Dynamic {
     Player{facing: Side, moving: Option<Side>, bounds: Rect},
     Enemy{facing: Side, kind: Enemy, bounds: Rect},
     Grease{facing: Side, spill: Option<NonZeroU16>, bounds: Rect},
 }
 
+#[derive(Debug, Clone, Copy)]
 pub enum Entrance {
     Flying(Side),
     Appearing(u8),
@@ -37,12 +43,23 @@ impl Room {
             facing: facing,
             player_h: x,
             player_v: y,
+            motion_h: 0,
+            motion_v: 0,
         }
     }
 }
 
 impl<'a> Play<'a> {
-    pub fn frame(&mut self, _actions: &[Input]) -> Outcome {
+    pub fn frame(&mut self, actions: &[Input]) -> Outcome {
+        let mut motion = (0i16, 0i16);
+        for action in actions {
+            match action {
+                Input::Go(direction) => motion.0 += *direction * MAX_THRUST,
+                _ => ()
+            };
+        }
+        self.motion_h = motion.0;
+        self.player_h = self.player_h.saturating_add_signed(motion.0);
         Outcome::Continue
     }
 
@@ -51,7 +68,7 @@ impl<'a> Play<'a> {
             .map(|&index| &self.room.objects[index] )
     }
 
-    pub fn player(&self) -> ((u16, u16), Side) {
-        ((self.player_h, self.player_v), self.facing)
+    pub fn player(&self) -> ((u16, u16), Side, bool) {
+        ((self.player_h, self.player_v), self.facing, self.facing * self.motion_h < 0)
     }
 }
