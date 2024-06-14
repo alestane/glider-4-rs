03319ae08d1d613e17e521322b3fe9bc04be6f5a
alestane <nevin@playsmith.net>
2024-06-14 12:05:56 -0400

    Expanded test house data.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,7 +1,28 @@
+use std::ops::Range;
 use glider::Room;
-
 use crate::resources;
 
 pub fn new() -> Room {
-    Room::try_from((0, &resources::THE_HOUSE[1270..1584])).unwrap()
+    Room::try_from((0, &resources::THE_HOUSE[1270..1584][..76])).unwrap()
+}
+
+const HOUSE_HEADER: usize = 1270;
+const ROOM_SIZE: usize = 314;
+
+fn index(i: usize) -> Range<usize> {
+    let start = HOUSE_HEADER + i * ROOM_SIZE;
+    start..(start + ROOM_SIZE)
+}
+
+const fn limit(i: usize) -> usize { 58 + i * 16 }
+
+pub fn house() -> Box<[Room]> {
+    Box::new([
+    //    Room::try_from((0, &resources::THE_HOUSE[index(0)])).unwrap(),
+    //    Room::try_from((1, &resources::THE_HOUSE[index(1)])).unwrap(),
+    //    Room::try_from((2, &resources::THE_HOUSE[index(2)])).unwrap(),
+    //    Room::try_from((3, &resources::THE_HOUSE[index(3)])).unwrap(),
+    //    Room::try_from((4, &resources::THE_HOUSE[index(4)])).unwrap(),
+        Room::try_from((5, &resources::THE_HOUSE[index(5)][..limit(3)])).unwrap(),
+    ])
 }
