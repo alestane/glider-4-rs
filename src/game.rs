b0661381fdfd7dfd33d55bf3c012a156eefba494
alestane <nevin@playsmith.net>
2024-06-14 16:45:27 -0400

    Added burn animation. Reset animation on death.

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,21 @@
 use sdl2::{keyboard::{KeyboardState, Scancode}, render::Texture};
 use glider::{Entrance, Input, Outcome, Room, Side, Update};
-use crate::{draw::Scribe, SCREEN_HEIGHT, SCREEN_WIDTH};
+use crate::{atlas, draw::{Animations, Frame, Scribe}, SCREEN_HEIGHT, SCREEN_WIDTH};
 use std::time::{Duration, Instant};
 
 const FADE_IN: &[usize] = &[3, 4, 3, 4, 5, 4, 5, 6, 5, 6, 7, 6, 7, 8, 7, 8, 9];
 const FADE_OUT: &[usize] = &[9, 8, 9, 8, 7, 8, 7, 6, 7, 6, 5, 6, 5, 4, 5, 4, 3];
+
+fn animate_with<F: FnOnce() -> Frame>(list: &mut Animations, id: u8, loader: F) {
+	if !list.contains_key(&id) {list.insert(id, loader());}
+}
 
 pub fn run(context: &mut crate::App, theme: &Texture, room: &Room, target: Entrance) -> Result<(u32, Option<(i16, Entrance)>), ()> {
     let display = &mut context.display;
     let loader = display.texture_creator();
 
     let mut backdrop = loader.create_texture_target(None, SCREEN_WIDTH, SCREEN_HEIGHT).expect("Failed to create backdrop texture");
-    let _ = display.with_texture_canvas(&mut backdrop, 
+    let _ = display.with_texture_canvas(&mut backdrop,
         |display| {
             display.draw_wall(&theme, &room.tile_order);
             for object in room.objects.iter().filter(|&object| !object.dynamic()) {
@@ -22,7 +26,7 @@ pub fn run(context: &mut crate::App, theme: &Texture, room: &Room, target: Entra
     let mut play = room.start(target);
     if let Entrance::Spawn(..) = target { play.reset(Entrance::default()) };
     let mut animation = HashMap::<u8, Box<dyn Iterator<Item = usize>>>::new();
-    
+
     let mut last = Instant::now();
     'game: loop {
         while last.elapsed() < Duration::from_millis(33) {}
@@ -38,18 +42,22 @@ pub fn run(context: &mut crate::App, theme: &Texture, room: &Room, target: Entra
         if keys.is_scancode_pressed(Scancode::Right) {inputs.push(Input::Go(Side::Right))};
         if keys.is_scancode_pressed(Scancode::Left) {inputs.push(Input::Go(Side::Left))};
         let result = play.frame(&inputs);
-        
+
         match result {
             Outcome::Continue(updates) => {
                 for update in updates.into_iter().flatten() {
                     match update {
-                        Update::Fade(inout) => if !animation.contains_key(&0) {animation.insert(0u8, if inout {Box::new(FADE_IN.iter().cloned())} else {Box::new(FADE_OUT.iter().cloned())});}, 
+                        Update::Fade(inout) => animate_with(&mut animation, 0, || if inout {Box::new(FADE_IN.iter().cloned())} else {Box::new(FADE_OUT.iter().cloned())}),
+                        Update::Burn => animate_with(&mut animation, 0, || Box::new(atlas::BURN.cycle()) ),
                         _ => ()
                     }
                 }
                 display.draw_room(&play, &mut animation, &context.sprites, &backdrop)
             },
-            Outcome::Dead => play.reset(match target {Entrance::Flying(side, ..) => Entrance::Spawn(side), target => target}),
+            Outcome::Dead => {
+            	animation.remove(&0);
+            	play.reset(match target {Entrance::Flying(side, ..) => Entrance::Spawn(side), target => target})
+            }
             Outcome::Leave{score, destination} => return Ok((score, destination)),
             _ => ()
         }
