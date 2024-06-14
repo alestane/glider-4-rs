9057a593e63bd5bb255c1067716e2ef7c5ceb7f6
alestane <nevin@playsmith.net>
2024-06-14 13:02:27 -0400

    Animation and extended play handling

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,12 @@
-use sdl2::{render::Texture, keyboard::{KeyboardState, Scancode}};
-use glider::{Entrance, Input, Room, Side};
+use sdl2::{keyboard::{KeyboardState, Scancode}, render::Texture};
+use glider::{Entrance, Input, Outcome, Room, Side, Update};
 use crate::{draw::Scribe, SCREEN_HEIGHT, SCREEN_WIDTH};
+use std::time::{Duration, Instant};
 
-pub fn run(context: &mut crate::App, theme: Texture, room: &Room) {
+const FADE_IN: &[usize] = &[3, 4, 3, 4, 5, 4, 5, 6, 5, 6, 7, 6, 7, 8, 7, 8, 9];
+const FADE_OUT: &[usize] = &[9, 8, 9, 8, 7, 8, 7, 6, 7, 6, 5, 6, 5, 4, 5, 4, 3];
+
+pub fn run(context: &mut crate::App, theme: &Texture, room: &Room, target: Entrance) -> Result<(u32, Option<(i16, Entrance)>), ()> {
     let display = &mut context.display;
     let loader = display.texture_creator();
 
@@ -15,9 +19,13 @@ pub fn run(context: &mut crate::App, theme: Texture, room: &Room) {
             }
         }
     );
-    let mut play = room.start(Entrance::Flying(Side::Left), true, true);
+    let mut play = room.start(target);
+    if let Entrance::Spawn(..) = target { play.reset(Entrance::default()) };
+    let mut animation = HashMap::<u8, Box<dyn Iterator<Item = usize>>>::new();
     
+    let mut last = Instant::now();
     'game: loop {
+        while last.elapsed() < Duration::from_millis(33) {}
         let mut inputs = Vec::new();
         for event in context.events.poll_iter() {
             use sdl2::event::Event;
@@ -29,7 +37,46 @@ pub fn run(context: &mut crate::App, theme: Texture, room: &Room) {
         let keys = KeyboardState::new(&context.events);
         if keys.is_scancode_pressed(Scancode::Right) {inputs.push(Input::Go(Side::Right))};
         if keys.is_scancode_pressed(Scancode::Left) {inputs.push(Input::Go(Side::Left))};
-        play.frame(&inputs);
-        display.draw_room(&play, &context.sprites, &backdrop);
+        let result = play.frame(&inputs);
+        
+        match result {
+            Outcome::Continue(updates) => {
+                for update in updates.into_iter().flatten() {
+                    match update {
+                        Update::Fade(inout) => if !animation.contains_key(&0) {animation.insert(0u8, if inout {Box::new(FADE_IN.iter().cloned())} else {Box::new(FADE_OUT.iter().cloned())});}, 
+                        _ => ()
+                    }
+                }
+                display.draw_room(&play, &mut animation, &context.sprites, &backdrop)
+            },
+            Outcome::Dead => play.reset(match target {Entrance::Flying(side, ..) => Entrance::Spawn(side), target => target}),
+            Outcome::Leave{score, destination} => return Ok((score, destination)),
+            _ => ()
+        }
+        last = Instant::now();
     }
+    Err(())
+}
+
+use std::collections::HashMap;
+
+pub fn play(context: &mut crate::App, pics: &HashMap<usize, Texture>, house: &[Room]) -> Result<(u32, usize), ()> {
+    let mut score = 0u32;
+    let mut room = 0usize;
+    let mut arrive = Entrance::default();
+    while let (points, Some((next, at))) = {
+        eprintln!("Object count: {}", house[room as usize].objects.len());
+        run(context, &pics[&(house[room as usize].theme_index() as usize)], &house[room], arrive)?
+    } {
+        score += points;
+        (room, arrive) = match at {
+            Entrance::Flying(..) => {
+                let Some(index) = room.checked_add_signed(next as isize) else { return Err(()) };
+                if index >= house.len() { return Err(()) }
+                (index, at)
+            }
+            Entrance::Spawn(..) => (next as usize, at)
+        };
+    }
+    Ok((score, room))
 }
