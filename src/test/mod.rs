614edc2af43fb6b9047aea747bc4227476951111
alestane <nevin@playsmith.net>
2024-06-19 10:06:56 -0400

    Restored all rooms for test.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -18,12 +18,12 @@ const fn limit(i: usize) -> usize { 58 + i * 16 }
 
 pub fn house() -> Box<[Room]> {
     Box::new([
-    //    Room::try_from((0, &resources::THE_HOUSE[index(0)])).unwrap(),
-    //    Room::try_from((1, &resources::THE_HOUSE[index(1)])).unwrap(),
-    //    Room::try_from((2, &resources::THE_HOUSE[index(2)])).unwrap(),
-    //    Room::try_from((3, &resources::THE_HOUSE[index(3)])).unwrap(),
-    //    Room::try_from((4, &resources::THE_HOUSE[index(4)])).unwrap(),
-    //    Room::try_from((5, &resources::THE_HOUSE[index(5)])).unwrap(),
-        Room::try_from((6, &resources::THE_HOUSE[index(6)][..limit(7)])).unwrap(),
+        Room::try_from((1, &resources::THE_HOUSE[index(0)])).unwrap(),
+        Room::try_from((2, &resources::THE_HOUSE[index(1)])).unwrap(),
+        Room::try_from((3, &resources::THE_HOUSE[index(2)])).unwrap(),
+        Room::try_from((4, &resources::THE_HOUSE[index(3)])).unwrap(),
+        Room::try_from((5, &resources::THE_HOUSE[index(4)])).unwrap(),
+        Room::try_from((6, &resources::THE_HOUSE[index(5)])).unwrap(),
+        Room::try_from((7, &resources::THE_HOUSE[index(6)])).unwrap(),
     ])
 }
