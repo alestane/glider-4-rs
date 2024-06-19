bc41a75af5e354ed1589c36f6148b7ad279d02d5
alestane <nevin@playsmith.net>
2024-06-19 10:45:44 -0400

    Added image for wainscoting room.

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -3,11 +3,13 @@ pub mod color {
     use std::collections::HashMap;
     const SPRITES: &[u8] = include_bytes!("color/128.png");
     const ROOM_200: &[u8] = include_bytes!("color/200.png");
+    const ROOM_201: &[u8] = include_bytes!("color/201.png");
 
     pub fn assets() -> HashMap<usize, &'static [u8]> {
         let mut values = HashMap::<usize, &'static [u8]>::new();
         values.insert(128, SPRITES);
         values.insert(200, ROOM_200);
+        values.insert(201, ROOM_201);
         values
     }
 }
