5af5e2abdbb55c20dd13dcc6a3e330ba616cfa13
alestane <nevin@playsmith.net>
2024-06-20 16:06:23 -0400

    Added basement theme.

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -4,12 +4,14 @@ pub mod color {
     const SPRITES: &[u8] = include_bytes!("color/128.png");
     const ROOM_200: &[u8] = include_bytes!("color/200.png");
     const ROOM_201: &[u8] = include_bytes!("color/201.png");
+    const ROOM_202: &[u8] = include_bytes!("color/202.png");
 
     pub fn assets() -> HashMap<usize, &'static [u8]> {
         let mut values = HashMap::<usize, &'static [u8]>::new();
         values.insert(128, SPRITES);
         values.insert(200, ROOM_200);
         values.insert(201, ROOM_201);
+        values.insert(202, ROOM_202);
         values
     }
 }
