c8c4c28aede9542405ebef66e8a4be3fece85238
alestane <nevin@playsmith.net>
2024-07-05 16:52:21 -0400

    Restored rooms 1..3 for tests.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -20,7 +20,7 @@ const fn limit(i: usize) -> usize { 58 + i * 16 }
 pub fn house() -> Box<[Room]> {
     let mut zip = 1u16..;
     Box::new(
-        [0; 13].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
+        [0; 3].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
             Room::try_from((id, &resources::THE_HOUSE[index(id.get() as usize - 1)])).unwrap()
         )
     )
