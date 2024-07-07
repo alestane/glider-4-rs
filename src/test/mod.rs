cc5040cd969c20fb9d9604ddc8b97059d0c6d067
alestane <nevin@playsmith.net>
2024-07-07 12:02:26 -0400

    Started room 13.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -9,7 +9,7 @@ pub fn new() -> Room {
 const HOUSE_HEADER: usize = 1270;
 const ROOM_SIZE: usize = 314;
 
-pub const START: NonZero<u16> = NonZero::new(12).unwrap();
+pub const START: NonZero<u16> = NonZero::new(13).unwrap();
 
 fn index(i: usize) -> Range<usize> {
     let start = HOUSE_HEADER + i * ROOM_SIZE;
@@ -22,7 +22,7 @@ const fn limit(i: usize) -> usize { 58 + i * 16 }
 pub fn house() -> Box<[Room]> {
     let mut zip = 1u16..;
     Box::new(
-        [0; 12].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
+        [0; 13].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
             Room::try_from((id, &resources::THE_HOUSE[index(id.get() as usize - 1)])).unwrap()
         )
     )
