75fb62420e8f2547ddd9134faf0958bea9986e0c
alestane <nevin@playsmith.net>
2024-07-19 10:31:47 -0400

    Clean up test room list.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -2,10 +2,6 @@ use std::{num::NonZero, ops::Range};
 use glider::Room;
 use crate::resources;
 
-pub fn new() -> Room {
-    Room::try_from((const{NonZero::new(1u16).unwrap()}, &resources::THE_HOUSE[1270..1584][..76])).unwrap()
-}
-
 const HOUSE_HEADER: usize = 1270;
 const ROOM_SIZE: usize = 314;
 
@@ -22,7 +18,7 @@ const fn limit(i: usize) -> usize { 58 + i * 16 }
 pub fn house() -> Box<[Room]> {
     let mut zip = 1u16..;
     Box::new(
-        [0; 16].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
+        [0; 17].map(move |_| zip.next().and_then(NonZero::new).unwrap()).map(|id|
             Room::try_from((id, &resources::THE_HOUSE[index(id.get() as usize - 1)])).unwrap()
         )
     )
