2f6931a5c4b98b8e2c3b46690ae0f138589c6cac
alestane <nevin@playsmith.net>
2024-07-25 17:10:53 -0400

    Add room theme 203,

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,10 +1,10 @@
-
 pub mod color {
     use std::collections::HashMap;
-    const SPRITES: &[u8] = include_bytes!("color/128.png");
+    pub const SPRITES: &[u8] = include_bytes!("color/128.png");
     const ROOM_200: &[u8] = include_bytes!("color/200.png");
     const ROOM_201: &[u8] = include_bytes!("color/201.png");
     const ROOM_202: &[u8] = include_bytes!("color/202.png");
+    const ROOM_203: &[u8] = include_bytes!("color/203.png");
     const ROOM_204: &[u8] = include_bytes!("color/204.png");
     const ROOM_205: &[u8] = include_bytes!("color/205.png");
     const ROOM_206: &[u8] = include_bytes!("color/206.png");
@@ -19,6 +19,7 @@ pub mod color {
                 (200, ROOM_200),
                 (201, ROOM_201),
                 (202, ROOM_202),
+                (203, ROOM_203),
                 (204, ROOM_204),
                 (205, ROOM_205),
                 (206, ROOM_206),
@@ -33,3 +34,4 @@ pub mod color {
 pub const CIRCLE: &'static [u8] = include_bytes!("circle.raw");
 
 pub const THE_HOUSE: &'static [u8] = include_bytes!("The House");
+pub const TWO_HOUSE: &'static [u8] = include_bytes!("The House 2");
